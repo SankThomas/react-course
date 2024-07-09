50bbf15ffe662aebfacfb991dc342e0c8feb1588
sankthomas <thomassankara74@gmail.com>
2024-07-09 14:52:59 +0300

    data

---
+++ 8-password-generator/src/data.js
@@ -0,0 +1,4 @@
+export const nums = "0123456789";
+export const lowercase = "abcdefghijklmnopqrstuvwxyz";
+export const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export const syms = "~!@#$%^&*()_+}{|:?<>";
