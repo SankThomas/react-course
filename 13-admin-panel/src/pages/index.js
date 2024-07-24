c94aaa4eff914296c606223092bb2d676474f1d8
sankthomas <thomassankara74@gmail.com>
2024-07-24 19:27:57 +0300

    entry file for pages folder

---
+++ 13-admin-panel/src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import Stats from "./Stats";
+import Upgrades from "./Upgrades";
+import Posts from "./Posts";
+import Error from "./Error";
+
+export { Home, Stats, Upgrades, Posts, Error };
