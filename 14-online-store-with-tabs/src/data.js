392408c325331f0a67f098633f894c635653dd86
sankthomas <thomassankara74@gmail.com>
2024-07-24 19:30:07 +0300

    data to mimic an api

---
+++ 14-online-store-with-tabs/src/data.js
@@ -0,0 +1,72 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const items = [
+  {
+    id: uuidv4(),
+    title: "Samsung A-Series",
+    description:
+      "Bed uncommonly his discovered for estimating far. Equally he minutes my hastily. Up hung mr we give rest half. Painful so he an comfort is manners. Bed uncommonly his discovered for estimating far. Equally he minutes my hastily. Up hung mr we give rest half. Painful so he an comfort is manners.",
+    images: [
+      "./images/a-series-1.jpg",
+      "./images/a-series-2.jpg",
+      "./images/a-series-3.jpg",
+    ],
+  },
+  {
+    id: uuidv4(),
+    title: "Tecno Camon 19",
+    description:
+      "Bed uncommonly his discovered for estimating far. Equally he minutes my hastily. Up hung mr we give rest half. Painful so he an comfort is manners. Bed uncommonly his discovered for estimating far. Equally he minutes my hastily. Up hung mr we give rest half. Painful so he an comfort is manners.",
+    images: [
+      "./images/camon-19-1.png",
+      "./images/camon-19-2.png",
+      "./images/camon-19-3.png",
+    ],
+  },
+  {
+    id: uuidv4(),
+    title: "Samsung Galaxy Z",
+    description:
+      "Bed uncommonly his discovered for estimating far. Equally he minutes my hastily. Up hung mr we give rest half. Painful so he an comfort is manners. Bed uncommonly his discovered for estimating far. Equally he minutes my hastily. Up hung mr we give rest half. Painful so he an comfort is manners.",
+    images: [
+      "./images/galaxy-z-1.jpg",
+      "./images/galaxy-z-2.jpg",
+      "./images/galaxy-z-3.jpg",
+    ],
+  },
+  {
+    id: uuidv4(),
+    title: "iPhone 13 Promax",
+    description:
+      "Bed uncommonly his discovered for estimating far. Equally he minutes my hastily. Up hung mr we give rest half. Painful so he an comfort is manners. Bed uncommonly his discovered for estimating far. Equally he minutes my hastily. Up hung mr we give rest half. Painful so he an comfort is manners.",
+    images: [
+      "./images/iphone-13-promax-2.png",
+      "./images/iphone-13-promax-3.png",
+      "./images/iphone-13-promax-4.png",
+    ],
+  },
+  {
+    id: uuidv4(),
+    title: "iPhone SE",
+    description:
+      "Bed uncommonly his discovered for estimating far. Equally he minutes my hastily. Up hung mr we give rest half. Painful so he an comfort is manners. Bed uncommonly his discovered for estimating far. Equally he minutes my hastily. Up hung mr we give rest half. Painful so he an comfort is manners.",
+    images: [
+      "./images/iphone-se-1.jpg",
+      "./images/iphone-se-2.png",
+      "./images/iphone-se-3.png",
+      "./images/iphone-se-4.png",
+      "./images/iphone-se-5.png",
+    ],
+  },
+  {
+    id: uuidv4(),
+    title: "Samsung Galaxy S22",
+    description:
+      "Bed uncommonly his discovered for estimating far. Equally he minutes my hastily. Up hung mr we give rest half. Painful so he an comfort is manners. Bed uncommonly his discovered for estimating far. Equally he minutes my hastily. Up hung mr we give rest half. Painful so he an comfort is manners.",
+    images: [
+      "./images/s22-burgundy-1.jpg",
+      "./images/s22-burgundy-2.jpg",
+      "./images/s22-burgundy-3.jpg",
+    ],
+  },
+]
