e797977ac6e2914dc3c839ea0d742b58741646e4
Thomas Sankara <thomassankara74@gmail.com>
2024-07-28 13:14:47 +0300

    create express server

---
+++ 20-mern-stack-app/server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import records from "./routes/record.js";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
