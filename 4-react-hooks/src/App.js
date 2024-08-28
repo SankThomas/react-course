db7f347be952a89a8bc3332e28f7f840b00ff7b4
SankThomas <thomassankara74@gmail.com>
2024-08-28 14:12:01 +0300

    render hook examples

--- 4-react-hooks/src/App.js
+++ 4-react-hooks/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import State from "./hooks/State";
+import Effect from "./hooks/Effect";
+import Ref from "./hooks/Ref";
+import Home from "./components/Home";
+import Memo from "./hooks/Memo";
+import Callback from "./hooks/Callback";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Ref /> */}
+      {/* <State /> */}
+      <Effect />
+
+      <Home />
+      <Memo />
+      <Callback />
     </div>
   );
 }
-
-export default App;
